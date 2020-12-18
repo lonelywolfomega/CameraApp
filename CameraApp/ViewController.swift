37ab34422886aec10343de430556cadcb1700871
Okhunov Odil <odiljon.ahunov.1997@mail.ru>
2020-12-18 18:07:59 +0900

    Initial Commit

---
+++ CameraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by Okhunov Odil on 2020/12/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
